fa8e5eef0e4c9fd28c37f8bb464f8bc618becc96
Ramyadevi C <ramyachola11@gmail.com>
2026-09-07 12:06:47 +0530

    Time: 2 ms (83.03%), Space: 47.6 MB (16.57%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -4,20 +4,16 @@ class Solution {
         if (n < 3) {
             return false;
         }
-        
         int i = 0;
-        
-        while (i + 1 < n && arr[i] < arr[i + 1]) {
+        while(i+1<n && arr[i]<arr[i+1]){
             i++;
         }
-        
-        if (i == 0 || i == n - 1) {
+        if(i==0 || i==n-1){
             return false;
         }
-        
-        while (i + 1 < n && arr[i] > arr[i + 1]) {
+        while(i+1<n && arr[i]>arr[i+1]){
             i++;
         }
-        return i == n - 1;
+        return i==n-1;
     }
 }
